a7dde9f2289ce5e54159d717b2b6a795e40b9cfd
dremkay <dremkay71@gmail.com>
2022-07-26 15:42:38 +0100

    create todo backend almost completed!

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+todos=[]
 
 def index(request):
-    return render(request,'todo/index.html')
+    # print(request.method)
+    if request.method =='POST':
+        # print(request.POST['text'])
+        todos.append(request.POST['text'])
+        context={'todos':todos}
+        return render(request,'todo/index.html',context)
     
+    context={'todos':todos}
+    return render(request,'todo/index.html', context)
